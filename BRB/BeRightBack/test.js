eaa7ef32aa23de768b22a35b5a037c6432ec1cab
Jerome Francis Salazar <2162253@slu.edu.ph>
2018-04-09 17:00:34 +0800

    Added functionalities

--- BRB/BeRightBack/test.js
+++ BRB/BeRightBack/test.js
@@ -1,39 +1,35 @@
+<html>
+    <body>
+    <div id ="saveInput"></div>
+    </body>
 <script type="text/javascript">
-var db = opendatabase('brb','1.0','BRB data', 2*1024*1024);
+    var db = opendatabase('brb', '1.0', 'BRB data',  2 * 1024 * 1024);
+    var msg;
 
-function loadAll(){
+function loadAll() {
     db.transaction(function (tx) {
-        tx.executeSql('Create table if it doesn't exist(ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description DATETIME)');
-        tx.executeSql('Insert data (name, creationDate) VALUES (?,?)', ["User 1", new Data()]);
-        tx.executeSql('Insert data (name, creationDate) VALUES (?,?)', ["User 2", new Data(2018-04-08)]);
+        tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS(ID UNIQUE PRIMARY KEY AUTOINCREMENT, name TEXT)');
+        tx.executeSql('INSERT INTO LOGS (id, name, log) VALUES (1, "foobar")');
+        tx.executeSql('INSERT INTO LOGS (id, name, log) VALUES (2, "logmsg")');   msg = '<p>Log message created and row inserted.</p>';
+        document.querySelector('#saveInput').innerHTML = msg;
     });
-}
 
-function displayAll(){
     db.transaction(function (tx) {
-        tx.executeSql('Select * from data', [], function(tx, results){
-            var n =results.rows.length;
-            var s = '<table cellpadding="2 cellspacing="2" border="2">';
-            for (var i=0; i<n; i++) {
-                var brb = results.rows.item(i);
-                s += '<tr>';
-                s += '<td>' + brb.id + '</td>';
-                s += '<td>' + brb.name + '</td>';
-                s += '<td>' + brb.phone + '</td>';
-                s += '<td>' + brb.type + '</td>';
-                s += '<td>' + brb.number + '</td>';
-                s += '<td>' + brb.timein + '</td>';
-                s += '<td>' + brb.timeout + '</td>';
-                s += '<td>' + brb.price + '</td>';
-                s += '</tr>';
+        tx.executeSql('SELECT * FROM LOGS', [], function(tx, results){
+            var len = results.rows.length, i;
+            msg = "<p>Found rows: " + len + "</p>";
+            document.querySelector('#saveInput').innerHTML += msg;
+            
+            for (var i=0; i<len; i++) {
+                msg = "<p><b>" + results.rows.item(i).log + "</b></p>"
+                document.querySelector('#saveInput').innerHTML += msg;
             }
-                s += '</table'>;
-                document.getElementById(saveInput).innerHTML = s;
-            });
-        });
-    }
+        }, null);
+    });
+    
      
 function AddAll(){
+    'use strict';
     db.transaction(function (tx) {
         var name = document.getElementById('name').value;
         var phone = document.getElementById('phone').value;
@@ -42,8 +38,8 @@ function AddAll(){
         var timein = document.getElementById('timein').value;
         var timeout = document.getElementById('timeout').value;
         var price = document.getElementById('price').value;
-        tx.executeSql('Insert into data (name) values(?,?)', [], function('Insert into table (name)){
-    });
+        tx.executeSql('Insert into data (name) values(?,?)', [], function('Insert into table (name)')
+    );
 
 }
 
@@ -56,28 +52,15 @@ function AddAll(){
         var timein = document.getElementById('timein').value;
         var timeout = document.getElementById('timeout').value;
         var price = document.getElementById('price').value;
-        tx.executeSql('Insert into data (name) values(?,?)', [], function('Insert into table (name)){
-    });
-
-}
-function AddAll(){
-    db.transaction(function (tx) {
-        var name = document.getElementById('name').value;
-        var phone = document.getElementById('phone').value;
-        var type = document.getElementById('type').value;
-        var number = document.getElementById('number').value;
-        var timein = document.getElementById('timein').value;
-        var timeout = document.getElementById('timeout').value;
-        var price = document.getElementById('price').value;
-        tx.executeSql('Insert into data (name) values(?,?)', [], function('Insert into table (name)){
-    });
-
+        tx.executeSql('Insert into data (name) values(?,?)', [], function('Insert into table (name)
+    ));
 }
 
 function del(id) {
 db.transaction(function (tx){
-    var result = confirm(You're about to delete this. Are you sure?);
+    var result = confirm('You are about to delete this. Are you sure?');
     txt.executeSql('Delete data from table', [id], displayAll());
     }
 )};
 </script>
+        </html>
