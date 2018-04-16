26a082780d48f9ec9f8b805cc6699cd0f7e9bffb
Andres, Erythrina <2160051@slu.edu.ph>
2018-04-17 00:00:52 +0800

    Updated semi-working localStorage

--- BeRightBack/js/submit.js
+++ BeRightBack/js/submit.js
@@ -1,42 +1,44 @@
+    var table, row, cell1, cell2, cell3, cell4, cell5, cell6, a1, b1, c1, d1, e1, f1;    
+
+
     function myFunction() {
-        var table = document.getElementById("myTable");
-        var row = table.insertRow(1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
+        table = document.getElementById("myTable");
+        row = table.insertRow(1);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell4 = row.insertCell(3);
+        cell5 = row.insertCell(4);
+        cell6 = row.insertCell(5);
 		
-    if(typeof(Storage) !== "undefined"){
 		var a = document.getElementById("name");
         localStorage.setItem('name',a.value);
-        var a1 = localStorage.getItem('name');
+        a1 = localStorage.getItem('name');
         console.log(a1);
                           
         var b = document.getElementById("type");
         localStorage.setItem('type',b.value);
-        var b1 = localStorage.getItem('type');
+        b1 = localStorage.getItem('type');
         console.log(b1);
               
 		var c = document.getElementById("number");
         localStorage.setItem('number',c.value);
-        var c1 = localStorage.getItem('number');
+        c1 = localStorage.getItem('number');
         console.log(c1);
               
         var d = document.getElementById("timein");
         localStorage.setItem('timein',d.value);
-        var d1 = localStorage.getItem('timein');
+        d1 = localStorage.getItem('timein');
         console.log(d1);
                 
 		var e = document.getElementById("timeout");
         localStorage.setItem('timeout',e.value);
-        var e1 = localStorage.getItem('timeout');
+        e1 = localStorage.getItem('timeout');
         console.log(e1);
               
 		var f = document.getElementById("price");
         localStorage.setItem('price',f.value);
-        var f1 = localStorage.getItem('price');
+        f1 = localStorage.getItem('price');
         console.log(f1);
                                
         cell1.innerHTML = a1; 
@@ -44,11 +46,41 @@
 		cell3.innerHTML = c1;
 		cell4.innerHTML = d1;
 		cell5.innerHTML = e1;
-		cell6.innerHTML = f1;
-
-        return 'You have unsaved changes!'
-        } else{
-            console.log("Your browser does not support localStorage");
-        }   
+		cell6.innerHTML = f1;   
         
-        } 
+        }
+
+        function relFunction(){
+            if(document.getElementById("name").value != null){
+                a1 = localStorage.getItem('name');
+                          
+                b1 = localStorage.getItem('type');
+              
+                c1 = localStorage.getItem('number');
+
+                d1 = localStorage.getItem('timein');
+
+                e1 = localStorage.getItem('timeout');
+              
+                f1 = localStorage.getItem('price');
+                
+
+                table = document.getElementById("myTable");
+                row = table.insertRow(1);
+                cell1 = row.insertCell(0);
+                cell2 = row.insertCell(1);
+                cell3 = row.insertCell(2);
+                cell4 = row.insertCell(3);
+                cell5 = row.insertCell(4);
+                cell6 = row.insertCell(5);
+
+                cell1.innerHTML = a1; 
+                cell2.innerHTML = b1;
+                cell3.innerHTML = c1;
+                cell4.innerHTML = d1;
+                cell5.innerHTML = e1;
+                cell6.innerHTML = f1;
+            } else{
+                return alert("Nothing to load");
+            }   
+        }
