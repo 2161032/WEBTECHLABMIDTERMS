1a21a3d01b0d8ee047d1a0f39207ad1325b8bd5a
Andres, Erythrina <2160051@slu.edu.ph>
2018-04-16 14:20:39 +0800

    Debugged dropdown for type and number of ride

--- BeRightBack/js/submit.js
+++ BeRightBack/js/submit.js
@@ -1,13 +1,14 @@
-        function myFunction() {
-            var table = document.getElementById("myTable");
-            var row = table.insertRow(1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(4);
-            var cell6 = row.insertCell(5);
-		 
+    function myFunction() {
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+		
+    if(typeof(Storage) !== "undefined"){
 		var a = document.getElementById("name");
         localStorage.setItem('name',a.value);
         let a1 = localStorage.getItem('name');
@@ -43,6 +44,9 @@
 		cell3.innerHTML = c1;
 		cell4.innerHTML = d1;
 		cell5.innerHTML = e1;
-		cell6.innerHTML = f1;   
+		cell6.innerHTML = f1;
+        } else{
+            console.log("Your browser does not support localStorage");
+        }   
         
         } 
