9fa63e5ccbfa6e9c7476bae44181395e7292ea61
Andres, Erythrina <2160051@slu.edu.ph>
2018-04-27 12:07:04 +0800

    Added folder for timeout version

---
+++ BeRightBack-NixUpdate - TimeOut/js/bikedb.js
@@ -0,0 +1,116 @@
+          var shortName='BRB_db';
+          var version='0.1'
+          var displayName='BRB_Database';
+          var maxSize = 65536;  
+          db = openDatabase(shortName, version, displayName, maxSize);
+
+          function loadAll(){
+            db.transaction(function(transaction) {
+                transaction.executeSql (
+                ' CREATE TABLE IF NOT EXISTS bike_data ' +
+                ' (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+                ' bikename TEXT NOT NULL, ' + 
+                ' biketype NOT NULL, ' + 
+                ' bikenumber NOT NULL, ' + 
+                ' biketimein NOT NULL, ' + 
+                ' biketimeout NOT NULL, ' + 
+                ' ctr NOT NULL, ' +
+                ' bikeprice INTEGER NOT NULL);');
+            });
+          }   
+          
+            function displayAll(){        
+                db.transaction(function(transaction) {
+                    transaction.executeSql('SELECT * FROM bike_data', [], function(transaction,results) {
+                        var n_rows = results.rows.length;
+                        console.log("Number of users: "+n_rows+" ");
+                    });
+                }, error_log);
+            }
+
+        function addAll(){
+            db.transaction(function (transaction) {
+                var bikename = document.getElementById('bikename').value;
+                var biketype = document.getElementById('biketype').value;
+                var bikenumber = document.getElementById('bikenumber').value;
+                var biketimein = document.getElementById('biketimein').value;
+                var biketimeout = localStorage.getItem('biketimeout');
+                var bikeprice = document.getElementById('bikeprice').value;
+                if(biketimeout == '30MIN'){
+                    var strArr = biketimein.split(":");
+                    var hour = parseInt(strArr[0]);
+                    var min = parseInt(strArr[1]) + 30;
+
+                    if(min > 60){
+                        min = min - 60;
+                        hour++;
+                        var ctr = hour+':'+min;
+                    }else{
+                        var ctr = hour+':'+min;
+                    }
+                     
+                } else if(biketimeout == '1HR'){
+                    var strArr = biketimein.split(":");
+                    var hour = parseInt(strArr[0])+1;
+                    var min = parseInt(strArr[1]);
+                    var ctr = hour+':'+min;                    
+                }else if(biketimeout == '1HR 30MIN'){
+                    var strArr = biketimein.split(":");
+                    var hour = parseInt(strArr[0])+1;
+                    var min = parseInt(strArr[1]) + 30;
+
+                    if(min > 60){
+                        min = min - 60;
+                        hour++;
+                        var ctr = hour+':'+min;
+                    }else{
+                        var ctr = hour+':'+min;
+                    }                  
+                }else if(biketimeout == '2HRS'){
+                    var strArr = biketimein.split(":");
+                    var hour = parseInt(strArr[0])+2;
+                    var min = parseInt(strArr[1]);
+                    var ctr = hour+':'+min;                    
+                }else{
+                    var ctr = "Free Time";
+                }
+
+                transaction.executeSql('INSERT INTO bike_data(bikename, biketype, bikenumber, biketimein, biketimeout, ctr, bikeprice) values(?,?,?,?,?,?,?)', [bikename, biketype, bikenumber, biketimein, biketimeout, ctr, bikeprice], displayAll());
+                
+                document.getElementById('bikename').value = '';
+                document.getElementById('biketype').value = '';
+                document.getElementById('bikenumber').value = '';
+                document.getElementById('biketimein').value = '';
+                document.getElementById('biketimeout').value = '';
+                document.getElementById('bikeprice').value = '';
+            });
+        }
+        function refAll(){
+            db.transaction(function (transaction) {  
+            transaction.executeSql('SELECT * FROM bike_data', [], function (transaction, results) {  
+                var len = results.rows.length, i;  
+                var str = '';  
+                for (i = 0; i < len; i++) {  
+                str += "<tr>";  
+                str += "<td>" + results.rows.item(i).bikename + "</td>";  
+                str += "<td>" + results.rows.item(i).biketype + "</td>";
+                str += "<td>" + results.rows.item(i).bikenumber + "</td>";    
+                str += "<td>" + results.rows.item(i).biketimein + "</td>"; 
+                str += "<td>" + results.rows.item(i).biketimeout + "</td>";
+                str += "<td>" + results.rows.item(i).bikeprice + "</td>"; 
+                str += "<td>" + results.rows.item(i).ctr+ "</td>"; 
+                str += "</tr>";  
+                document.getElementById("myTable").innerHTML += str;  
+                str = '';  
+                }  
+            }, null);  
+        });  
+        }
+
+        function returnTime(elementID){
+            return localStorage.getItem(elementID);
+        }
+
+        function error_log(error) {
+            console.log(error.message);
+        }
---
+++ BeRightBack-NixUpdate - TimeOut/js/submitSkate.js
@@ -0,0 +1,156 @@
+    var table, row, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, a1, b1, c1, d1, e1, f1, g1, g, e, timerValue, tot, timeVal;
+    var ctr = 0;    
+    var count = 0;
+
+    function myFunction() {
+        table = document.getElementById("myTable");
+        row = table.insertRow(1);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell4 = row.insertCell(3);
+        cell5 = row.insertCell(4);
+        cell6 = row.insertCell(5);
+        cell7 = row.insertCell(6);
+        cell7.id = ctr;
+        /*cell8 = row.insertCell(7);
+        cell8.id = count;*/
+
+        
+        var a = document.getElementById("skatename");
+        localStorage.setItem('skatename',a.value);
+        a1 = localStorage.getItem('skatename');
+        console.log(a1);
+                          
+        var b = document.getElementById("skatetype");
+        localStorage.setItem('skatetype',b.value);
+        b1 = localStorage.getItem('skatetype');
+        console.log(b1);
+              
+        var c = document.getElementById("skatenumber");
+        localStorage.setItem('skatenumber',c.value);
+        c1 = localStorage.getItem('skatenumber');
+        console.log(c1);
+              
+        var d = document.getElementById("skatetimein");
+        localStorage.setItem('skatetimein',d.value);
+        d1 = localStorage.getItem('skatetimein');
+        console.log(d1);
+                
+        e = document.getElementById("skatetimeout");
+        if(e.value == 1800){
+            timeVal = "30MIN";
+        } else if(e.value == 3600){
+            timeVal = "1HR";
+        }else if(e.value == 5400){
+            timeVal = "1HR 30MIN";
+        }else if(e.value == 7200){
+            timeVal = "2HRS";
+        }else{
+            timeVal = "FREE TIME";
+        }
+
+        localStorage.setItem('skatetimeout', timeVal);
+        e1 = localStorage.getItem('skatetimeout');
+        console.log(e1);
+              
+        var f = document.getElementById("skateprice");
+        localStorage.setItem('skateprice',f.value);
+        f1 = localStorage.getItem('skateprice');
+        console.log(f1);
+                               
+        cell1.innerHTML = a1; 
+        cell2.innerHTML = b1;
+        cell3.innerHTML = c1;
+        cell4.innerHTML = d1;
+        cell5.innerHTML = e1;
+        cell6.innerHTML = f1;
+        if(timeVal == "FREE TIME"){
+            cell7.innerHTML = 'FREE TIME';
+        } else{
+            if(timeVal == '30MIN'){
+                    var strArr = d1.split(":");
+                    var hour = parseInt(strArr[0]);
+                    var min = parseInt(strArr[1]) + 30;
+
+                    if(min > 60){
+                        min = min - 60;
+                        hour++;
+                        cell7.innerHTML = hour+':'+min;
+                    }else{
+                        cell7.innerHTML = hour+':'+min;
+                    }
+                     
+                } else if(timeVal == '1HR'){
+                    var strArr = d1.split(":");
+                    var hour = parseInt(strArr[0])+1;
+                    var min = parseInt(strArr[1]);
+                    cell7.innerHTML = hour+':'+min;                    
+                }else if(timeVal == '1HR 30MIN'){
+                    var strArr = d1.split(":");
+                    var hour = parseInt(strArr[0])+1;
+                    var min = parseInt(strArr[1]) + 30;
+
+                    if(min > 60){
+                        min = min - 60;
+                        hour++;
+                        cell7.innerHTML = hour+':'+min;
+                    }else{
+                        var ctr = hour+':'+min;
+                    }                  
+                }else if(timeVal == '2HRS'){
+                    var strArr = d1.split(":");
+                    var hour = parseInt(strArr[0])+2;
+                    var min = parseInt(strArr[1]);
+                    cell7.innerHTML = hour+':'+min;                    
+                }           
+        }
+
+        //cell8.innerHTML = 'Stop';
+        count++;
+        }
+
+        function relFunction(){
+            if(localStorage.getItem('skatename') != null){
+                a1 = localStorage.getItem('skatename');                          
+                b1 = localStorage.getItem('skatetype');              
+                c1 = localStorage.getItem('skatenumber');
+                d1 = localStorage.getItem('skatetimein');
+                e1 = localStorage.getItem('skatetimeout');              
+                f1 = localStorage.getItem('skateprice');               
+                tot = localStorage.getItem('skatetotal');
+
+                //cell8 = row.insertCell(7);
+                     
+                //cell8.innerHTML = 'Stop';
+                document.getElementById('skatetotal').innerHTML = tot;
+            } else{
+                return alert("Nothing to load");
+            }   
+        }
+
+        var total = 0;
+                  
+        //document.getElementById('saveInput').onsubmit;
+
+        function myEntryFunction() {
+            var entry = document.getElementById('skateprice').value;
+            var entry = parseFloat(entry);
+                currency = currencyFormat(entry);
+                document.getElementById('skatemyEntries').innerHTML += '<tr><td></td><td>' + currency + '</td></tr>';
+                total += entry;
+                localStorage.setItem('skatetotal', total);
+                document.getElementById('skatetotal').innerHTML = currencyFormat(total);
+                return false;
+        }
+
+        function currencyFormat(number) {
+            var currency = parseFloat(number);
+            currency = currency.toFixed(2);
+            currency = 'P' + currency;
+            return currency;
+        }
+
+        
+
+       
